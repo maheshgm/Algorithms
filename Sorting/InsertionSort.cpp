7e54a0117d8355278b0352c809e49e5bcef190e7
maheshgm <gmahesh9391@gmail.com>
2020-05-29 08:26:16 +0530

    Initial commit

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -4,20 +4,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 void insertionSort(vector<int> &A,int n){
-	for(int i=0;i<n-1;i++){
-		if(A[i]>A[i+1]){
-			int j=i;
-			int key=A[i+1];
-			while(j>0 && A[j]>key){
-				A[j-1]=A[j];
-				j--;
-			}
-			for(int k:A){
-				cout<<k<<" ";
-			}
-			cout<<endl;
-			A[j]=key;
+	for(int i=1;i<n;i++){
+		int j=i-1;
+		int key=A[i];
+		while(j>=0 && key<A[j]){
+			A[j+1]=A[j];
+			j--;
 		}
+		A[j+1]=key;
 	}
 }
 
